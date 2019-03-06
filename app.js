6fd3846026e3ba25f97f30c3402f81fdedf72a29
Alin Tudosie <alin.tudosie@itsix.com>
2019-03-06 15:55:08 +0200

    Added passport authentication

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
 
 const app = express();
+
+// Passport config
+
+require('./config/passport')(passport);
 
 // DB Config
 const db = require('./config/keys').MongoURI;
@@ -20,7 +28,30 @@ app.set('view engine', 'ejs');
 // Bodyparser
 app.use(express.urlencoded({extended: false}));
 
-//Routes
+
+// Express session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect Flash
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+// Routes
 app.use('/', require('./routes/index'))
 
 app.use('/users', require('./routes/users'))
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 
 // User model
@@ -65,11 +66,48 @@ router.post('/register', (req, res) => {
                     email,
                     password
                 })
-                console.log(newUser);
-                res.send('Hello')
+                // console.log(newUser);
+                // res.send('Hello')
+                
+                // Hash password
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        
+                        // Set password to hash
+                        newUser.password = hash;
+
+                        // Save the user
+                        newUser.save()
+                            .then(user => {
+                                req.flash('success_msg', 'You are now registered and can log in');
+                                res.redirect('/users/login');
+                            })
+                            .catch(err => console.log(err));
+                            
+                    })
+                })
             }
         })
     }
 })
 
+// Login Handle
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
+
+
+// Logout Handle
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login')
+})
 module.exports = router;
